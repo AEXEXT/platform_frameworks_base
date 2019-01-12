8bb09ccd05cf7958e3fec246e110a3cfa770c8a3
Rostyslav <rostifaner@gmail.com>
2019-01-12 13:01:18 +0200

    base: Fix compiling

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1156,8 +1156,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ALLOW_INCALL_HOME), false, this,
                     UserHandle.USER_ALL);                    
-                    Settings.System.RECENTS_LAYOUT_STYLE), false, this,
-                    UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.OMNI_USE_BOTTOM_GESTURE_NAVIGATION), false, this,
                     UserHandle.USER_ALL);
