61f6e18fb25117711c229256a311a6f9d9cc6da3
sileshn <sileshn@gerrit.aospextended.com>
2019-02-14 15:29:26 +0100

    Revert "SystemUI: Fix an issue where jumping to black after screen turned on"
    This reverts commit c8f3d971d279eeacbb69e113173d724f6ccaf9cf.
    
    This breaks ambient notifications as well as handwave/pickup pulse notifications.
    
    Change-Id: I349820b6a2dd0cbd37f5e3f79e333e456c300e97

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5248,7 +5248,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         @Override
         public void startDozing() {
-             if (!mDozingRequested && !isScreenTurningOnOrOn()) {
+            if (!mDozingRequested) {
                 mDozingRequested = true;
                 DozeLog.traceDozing(mContext, mDozing);
                 updateDozing();
@@ -5309,9 +5309,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 DozeLog.traceDozing(mContext, mDozing);
                 mWakefulnessLifecycle.dispatchStartedWakingUp();
                 updateDozing();
-
-                updateScrimController();
-           }
+            }
         }
 
         @Override
